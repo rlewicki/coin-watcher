c560696207ffdfcfd7124f8d6f90adc45d6b6b76
rlewicki <zguba44@gmail.com>
2017-08-03 19:40:34 +0200

    Created HttpGetter unit tests

---
+++ app/src/test/java/pw/robertlewicki/coinwatcher/HttpGetterTest.java
@@ -0,0 +1,17 @@
+package pw.robertlewicki.coinwatcher;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import pw.robertlewicki.coinwatcher.Util.HttpGetter;
+
+public class HttpGetterTest {
+
+    @Test
+    public void getHttpRequestShouldReturnNonEmptyString() throws Exception {
+        HttpGetter httpGetter = new HttpGetter();
+        String url = "http://httpbin.org/ip";
+        assertNotEquals(httpGetter.getResponse(url), "");
+    }
+}
