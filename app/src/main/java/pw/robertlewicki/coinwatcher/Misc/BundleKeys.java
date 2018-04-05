78eb0402683ce927667de83c6b775d089dd0dec7
rlewicki <zguba44@gmail.com>
2018-04-06 00:19:31 +0200

    Added new keys to BundleKeys

--- app/src/main/java/pw/robertlewicki/coinwatcher/Misc/BundleKeys.java
+++ app/src/main/java/pw/robertlewicki/coinwatcher/Misc/BundleKeys.java
@@ -5,6 +5,10 @@ public class BundleKeys
     public static String RANK = "rank";
     public static String FULL_NAME = "full_name";
     public static String PRICE_USD = "price_usd";
+    public static String PRICE_BTC = "price_btc";
+    public static String HOURLY_PERCENT_CHANGE = "hourly_percent_change";
+    public static String DAILY_PERCENT_CHANGE = "daily_percent_change";
+    public static String WEEKLY_PERCENT_CHANGE = "weekly_percent_change";
     public static String DAILY_VOLUME = "daily_volume";
     public static String MARKET_CAP = "market_cap";
     public static String AVAILABLE_SUPPLY = "available_supply";
